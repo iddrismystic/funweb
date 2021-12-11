c6141040dcb829eef11c3ec31d6aee23094d8db4
iddrisabdulwahab <79385555+iddrismystic@users.noreply.github.com>
2021-12-11 03:09:18 +0000

    ads

--- components/Layout.js
+++ components/Layout.js
@@ -8,6 +8,8 @@ const Layout = ({children}) => {
     return (  
         <div  lang="en">
       <Script src="https://fun-css.github.io/funcss/js/fun.js" />
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6351985525676458" crossorigin="anonymous" />
+ 
             <Navbar />
         <div className="Page">
             <Sidebar />
